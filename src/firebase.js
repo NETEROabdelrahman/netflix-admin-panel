2011e65facfd0de75f5b8ece2aa388be9563c716
Abdelrahman <114447180+NETEROabdelrahman@users.noreply.github.com>
2023-04-20 04:58:18 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,1 @@
-import firebase from "firebase"
-
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBvyFKUAAZEWNfddrQFusuoLnPBuMdqsBU",
-    authDomain: "netflix-clone-52090.firebaseapp.com",
-    projectId: "netflix-clone-52090",
-    storageBucket: "netflix-clone-52090.appspot.com",
-    messagingSenderId: "389607862759",
-    appId: "1:389607862759:web:d24b443fa64d34540a4d42",
-    measurementId: "G-35R97KBR4R"
-};
-  
-
-firebase.initializeApp(firebaseConfig)
-const storage = firebase.storage()
-export default storage;
+kk
